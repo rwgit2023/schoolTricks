313e51d9be3f8a1fa8b83db9e7e14fe8a2d87fd6
rwgit2023 <ronangit2023@hotmail.com>
2023-04-04 16:00:36 -0300

    Thirteenth Commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,16 @@
+const botaoEnviar = document.querySelector('.enviar')
+
+const professora = document.getElementById("professora");
+const modal = document.querySelector(".modal");
+console.log(professora.src)
 
 
 
 
 
 
+botaoEnviar.addEventListener("click",() =>{
+    console.log("Cliquei no enviar")
+
+    modal.classList("aberto");
+})
