21bfe6f1cf4c408334c1ef78865584bf1450a626
rwgit2023 <ronangit2023@hotmail.com>
2023-04-06 14:24:43 -0300

    Fifteenth Commit

--- src/js/main.js
+++ src/js/main.js
@@ -7,11 +7,40 @@ console.log(professora.src)
 
 
 
+botaoEnviar.addEventListener('click', function(event) {
 
+    event.preventDefault();
 
-botaoEnviar.addEventListener("click",() =>{
+// botaoEnviar.addEventListener("click",() =>{
+
+    
     console.log("Cliquei no enviar")
 
+
     modal.classList.add("aberto");
-    professora.setAttribute("src", linkDoVideo);
+    professora.setAttribute("src", linkImagem);
+
+    // Seleciona o valor do campo de entrada de texto
+    const valorNomeDigitado = document.querySelector('#nome_sobrenome').value;
+    // Seleciona o valor do campo de entrada de texto
+    const valoIidadeDigitado = document.getElementById('idade');
+    // Seleciona o elemento de exibição de resultado
+    const resultado = document.getElementById('resultado');
+    
+    // Exibe o valor digitado na tela
+    resultado.textContent = `Olá ${valorNomeDigitado}, você quer participar do meu desafio? Tenho certeza que vai arrazar`;
+    console.log(resultado)
 })
+
+  
+  
+
+  
+  
+
+  
+  
+
+  
+  
+  
