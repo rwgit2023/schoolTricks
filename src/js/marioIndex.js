f0596bc74409ebb4214fd2af3ada3c6003a3cdc9
rwgit2023 <ronangit2023@hotmail.com>
2023-04-19 14:54:46 -0300

    twenty-sixth Commit

--- src/js/marioIndex.js
+++ src/js/marioIndex.js
@@ -1,36 +1,55 @@
  // Apresenta o 1 numero no campo
- const num1 = document.getElementById('num1');
- const num2 = document.getElementById('num2');
- const num3 = document.getElementById('num3');
- const num4 = document.getElementById('num4');
- const num5 = document.getElementById('num5');
- const num6 = document.getElementById('num6');
- const num7 = document.getElementById('num7');
- const num8 = document.getElementById('num8');
+ const spot1 = document.getElementById('num1');
+ const spot2 = document.getElementById('num2');
+ const spot3 = document.getElementById('num3');
+ const spot4 = document.getElementById('num4');
+ const spot5 = document.getElementById('num5');
+ const spot6 = document.getElementById('num6');
+ const spot7 = document.getElementById('num7');
+ const spot8 = document.getElementById('num8');
 
  
  // Exibe o valor digitado na tela
 
 
+ 
 
+num1 = (Math.floor(Math.random() * 100) + 1) 
+num2 = (Math.floor(Math.random() * 100) + 1)
+spot1.textContent = (num1 + '      +      ' +num2 )
+R1 = num1 + num2
 
-//  let numeroAleatorio = (Math.floor(Math.random() * 100) + 1);
-//  console.log(numeroAleatorio);
+num3 = (Math.floor(Math.random() * 100) + 1) 
+num4 = (Math.floor(Math.random() * 100) + 1)
+spot2.textContent = (num3 + '      +      ' +num4 )
+R2 = num1 + num2
 
+num5 = (Math.floor(Math.random() * 100) + 1) 
+num6 = (Math.floor(Math.random() * 100) + 1)
+spot3.textContent = (num5 + '      +      ' +num6 )
+R3 = num1 + num2
 
-num1.textContent = (Math.floor(Math.random() * 100) + 1) + '      +      ' + (Math.floor(Math.random() * 100) + 1)
+num7 = (Math.floor(Math.random() * 100) + 1) 
+num8 = (Math.floor(Math.random() * 100) + 1)
+spot4.textContent = (num7 + '      +      ' +num8 )
+R4 = num1 + num2
 
-num2.textContent = (Math.floor(Math.random() * 100) + 1) + '      +      ' + (Math.floor(Math.random() * 100) + 1)
+num9 = (Math.floor(Math.random() * 100) + 1) 
+num10 = (Math.floor(Math.random() * 100) + 1)
+spot5.textContent = (num9 + '      +      ' +num10 )
+R5 = num1 + num2
 
-num3.textContent = (Math.floor(Math.random() * 100) + 1) + '      +      ' + (Math.floor(Math.random() * 100) + 1)
+num11 = (Math.floor(Math.random() * 100) + 1) 
+num12 = (Math.floor(Math.random() * 100) + 1)
+spot6.textContent = (num11 + '      +      ' +num12 )
+R6 = num1 + num2
 
-num4.textContent = (Math.floor(Math.random() * 100) + 1) + '      +      ' + (Math.floor(Math.random() * 100) + 1)
+num13 = (Math.floor(Math.random() * 100) + 1) 
+num14 = (Math.floor(Math.random() * 100) + 1)
+spot7.textContent = (num13 + '      +      ' +num14 )
+R7 = num1 + num2
 
-num5.textContent = (Math.floor(Math.random() * 100) + 1) + '      +      ' + (Math.floor(Math.random() * 100) + 1)
-
-num6.textContent = (Math.floor(Math.random() * 100) + 1) + '      +      ' + (Math.floor(Math.random() * 100) + 1)
-
-num7.textContent = (Math.floor(Math.random() * 100) + 1) + '      +      ' + (Math.floor(Math.random() * 100) + 1)
-
-num8.textContent = (Math.floor(Math.random() * 100) + 1) + '      +      ' + (Math.floor(Math.random() * 100) + 1)
-
+num15 = (Math.floor(Math.random() * 100) + 1) 
+num16 = (Math.floor(Math.random() * 100) + 1)
+spot8.textContent = (num15 + '      +      ' +num16 )
+R8 = num1 + num2
