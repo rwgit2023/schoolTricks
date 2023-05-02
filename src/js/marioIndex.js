13aa0b0fcaca0c6a461899edf18901b677b01e1d
rwgit2023 <ronangit2023@hotmail.com>
2023-05-02 14:25:15 -0300

    twenty-seventh COmmit

--- src/js/marioIndex.js
+++ src/js/marioIndex.js
@@ -17,39 +17,61 @@
 num1 = (Math.floor(Math.random() * 100) + 1) 
 num2 = (Math.floor(Math.random() * 100) + 1)
 spot1.textContent = (num1 + '      +      ' +num2 )
-R1 = num1 + num2
+var correto1 = num1 + num2
 
 num3 = (Math.floor(Math.random() * 100) + 1) 
 num4 = (Math.floor(Math.random() * 100) + 1)
 spot2.textContent = (num3 + '      +      ' +num4 )
-R2 = num1 + num2
+var correto2 = num3 + num4
 
 num5 = (Math.floor(Math.random() * 100) + 1) 
 num6 = (Math.floor(Math.random() * 100) + 1)
 spot3.textContent = (num5 + '      +      ' +num6 )
-R3 = num1 + num2
+var correto3 = num5 + num6
 
 num7 = (Math.floor(Math.random() * 100) + 1) 
 num8 = (Math.floor(Math.random() * 100) + 1)
 spot4.textContent = (num7 + '      +      ' +num8 )
-R4 = num1 + num2
+var correto4 = num7 + num8
 
 num9 = (Math.floor(Math.random() * 100) + 1) 
 num10 = (Math.floor(Math.random() * 100) + 1)
 spot5.textContent = (num9 + '      +      ' +num10 )
-R5 = num1 + num2
+var correto5 = num9 + num10
 
 num11 = (Math.floor(Math.random() * 100) + 1) 
 num12 = (Math.floor(Math.random() * 100) + 1)
 spot6.textContent = (num11 + '      +      ' +num12 )
-R6 = num1 + num2
+var correto6 = num11 + num12
 
 num13 = (Math.floor(Math.random() * 100) + 1) 
 num14 = (Math.floor(Math.random() * 100) + 1)
 spot7.textContent = (num13 + '      +      ' +num14 )
-R7 = num1 + num2
+var correto7 = num13 + num14
 
 num15 = (Math.floor(Math.random() * 100) + 1) 
 num16 = (Math.floor(Math.random() * 100) + 1)
 spot8.textContent = (num15 + '      +      ' +num16 )
-R8 = num1 + num2
+var correto8 = num15 + num16
+
+i = 0
+
+// Captura resultados digitados
+ resultado1 = document.querySelector('#resp1_1').value;
+ resultado2 = document.querySelector('#resp2_2').value;
+ resultado3 = document.querySelector('#resp3_3').value;
+ resultado4 = document.querySelector('#resp4_4').value;
+ resultado5 = document.querySelector('#resp5_5').value;
+ resultado6 = document.querySelector('#resp6_6').value;
+ resultado7 = document.querySelector('#resp7_7').value;
+ resultado8 = document.querySelector('#resp8_8').value;
+
+let lista1 = []
+lista1.push(correto1, correto2, correto3, correto4, correto5, correto6, correto7, correto8)
+console.log(lista1)
+
+lista2 = []
+lista2 .push(resultado1, resultado2, resultado3, resultado4, resultado5, resultado6, resultado7, resultado8)
+console.log(lista2)
+
+ 
