d1f106beda7253dce4d2ab7dea15920f5f6bfb5a
rwgit2023 <ronangit2023@hotmail.com>
2023-05-04 15:01:26 -0300

    twenty-ninth Commit

--- src/js/marioIndex.js
+++ src/js/marioIndex.js
@@ -57,39 +57,76 @@ var correto8 = num15 + num16
 i = 0
 
 // Captura resultados digitados
- resultado1 = document.querySelector('#resp1_1').value;
- resultado2 = document.querySelector('#resp2_2').value;
- resultado3 = document.querySelector('#resp3_3').value;
- resultado4 = document.querySelector('#resp4_4').value;
- resultado5 = document.querySelector('#resp5_5').value;
- resultado6 = document.querySelector('#resp6_6').value;
- resultado7 = document.querySelector('#resp7_7').value;
- resultado8 = document.querySelector('#resp8_8').value;
-
-let lista1 = []
-lista1.push(correto1, correto2, correto3, correto4, correto5, correto6, correto7, correto8)
-console.log(lista1)
-
-lista2 = []
-lista2 .push(resultado1, resultado2, resultado3, resultado4, resultado5, resultado6, resultado7, resultado8)
-console.log(lista2)
 
 
-let i = 0;
-let pontos = 0;
 
 
-const botaoEnviar = document.querySelector('.calcular')
 
- botaoEnviar.addEventListener('click', function(event) {
 
-    while (i < lista1.length && i < lista2.length) {
-        if (lista1[i] === lista2[i]) {
-          pontos += 10; // soma 10 pontos para cada posição igual
-        } else {
-          pontos -= 5; // subtrai 5 pontos para cada posição diferente
-        }
-        i++;
+
+
+
+
+
+//  -------------------------------------------------------------------------------------------
+// 
+function calcular() {
+
+  resultado1 = document.querySelector('#resp1_1').value;
+  resultado2 = document.querySelector('#resp2_2').value;
+  resultado3 = document.querySelector('#resp3_3').value;
+  resultado4 = document.querySelector('#resp4_4').value;
+  resultado5 = document.querySelector('#resp5_5').value;
+  resultado6 = document.querySelector('#resp6_6').value;
+  resultado7 = document.querySelector('#resp7_7').value;
+  resultado8 = document.querySelector('#resp8_8').value;
+ 
+  let lista1 = []
+  lista1.push(correto1, correto2, correto3, correto4, correto5, correto6, correto7, correto8)
+  console.log('Respostas Corretas:', lista1)
+  
+  lista2 = []
+  lista2 .push(resultado1, resultado2, resultado3, resultado4, resultado5, resultado6, resultado7, resultado8)
+  console.log('Respostas:',lista2)
+  
+  lista_erros = []
+  lista_acertos = []
+
+  let i = 0;
+  let pontos = 0;
+
+  while (i < lista1.length && i < lista2.length) {
+    if (lista1[i] == lista2[i]) {
+      pontos += 2; // soma 10 pontos para cada posição igual
+      lista_acertos.push(lista1[i])
+
+
+    } else {
+      pontos -= 1; // subtrai 5 pontos para cada posição diferente
+      lista_erros.push(lista1[i])
+      if (pontos <=0){
+        pontos = 0
       }
+    }
+    i++;
+  }
 
- })
+  console.log(pontos); // exibe o total de pontos obtidos
+  console.log('Você acertou as questões: ', lista_acertos)
+  console.log('Você errou as questões: ', lista_erros)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
